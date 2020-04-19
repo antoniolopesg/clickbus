f6071961392753f13382b3c640aded9f21befd8f
antoniolopesg <antoniolopesg@protonmail.com>
2020-04-19 16:14:10 -0300

    :white_check_mark: duplicate places

--- __tests__/integration/place.spec.ts
+++ __tests__/integration/place.spec.ts
@@ -1,15 +1,35 @@
 import request from 'supertest';
 import app from '../../src/app';
+import conn from '../../src/database/conn';
 
-describe('User', () => {
+describe('Place', () => {
+  beforeEach(async () => {
+    await conn('places').truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app).post('/places').send({
       name: 'Teresina Shopping',
-      slug: 'teresina-shopping',
       city: 'teresina',
       state: 'PI',
     });
 
     expect(response.status).toBe(201);
+  });
+
+  it('should not be able to register duplicate places', async () => {
+    await request(app).post('/places').send({
+      name: 'Teresina Shopping',
+      city: 'teresina',
+      state: 'PI',
+    });
+
+    const response = await request(app).post('/places').send({
+      name: 'Teresina Shopping',
+      city: 'teresina',
+      state: 'PI',
+    });
+
+    expect(response.status).toBe(400);
   });
 });
--- src/controllers/PlaceController.ts
+++ src/controllers/PlaceController.ts
@@ -3,8 +3,27 @@ import conn from '../database/conn';
 
 class PlaceController {
   async create(req: Request, res: Response) {
+    const { name, city, state } = req.body;
+
     try {
-      const [id] = await conn('places').insert(req.body).returning('id');
+      const place = await conn('places')
+        .select('id')
+        .where('name', name)
+        .andWhere('city', city)
+        .andWhere('state', state)
+        .first();
+
+      if (place) {
+        return res
+          .status(400)
+          .json({ error: 'this place has already been registered' });
+      }
+
+      const slug = name.toLowerCase().replace(/ /g, '-');
+
+      const [id] = await conn('places')
+        .insert({ name, slug, city, state })
+        .returning('id');
 
       return res.status(201).json({ id });
     } catch (err) {
