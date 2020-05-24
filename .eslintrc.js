4a93a7bf8ba1c23b11f8252fa4bb8775dbf7036b
Antonio Lopes <antoniolopesg@protonmail.com>
2020-05-23 23:28:41 -0300

    change eslint style guide airbnb->standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,24 @@
 module.exports = {
   env: {
     es6: true,
-    node: true,
+    node: true
   },
   extends: [
-    'airbnb-base',
+    'standard',
     'prettier'
   ],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
+    ecmaVersion: 11,
+    sourceType: 'module'
   },
   plugins: [
-    '@typescript-eslint',
-    'prettier'
+    '@typescript-eslint'
   ],
   rules: {
-    'class-methods-use-this': 'off',
-    'no-param-reassign': 'off',
-    'camelcase': 'off',
-    'no-unused-vars': 'off',
-    'prettier/prettier': 'error',
-    'import/extensions': 'off',
-    'no-undef': 'off',
-    'import/no-unresolved': 'off'
-  },
-};
+  }
+}
